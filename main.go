7411bcee985225d2fbb0b21438969f2a987c506d
jessicapwx <jessica@portworx.com>
2017-09-19 11:41:55 +1300

    Rebootnode in single go func

--- main.go
+++ main.go
@@ -51,5 +51,31 @@ func main() {
 		fmt.Println("there was an error listing instances in", awsRegion, err.Error())
 		log.Fatal(err.Error())
 	}
-	fmt.Printf("%+v\n", *resp)
+	//fmt.Printf("%+v\n", *resp)
+	reservations := resp.Reservations
+	fmt.Printf("%+v\n", reservations)
+	for i, resv := range reservations {
+		fmt.Printf("reservation %d: %+v\n", i, resv)
+	}
+	for i, ins := range reservations[0].Instances {
+		fmt.Printf("\ninstance %d: %+v\n", i, ins)
+		fmt.Printf("\ninstance %d id: %s\n", i, *(ins.InstanceId))
+		fmt.Printf("\ninstance %d PublicIpAddress: %s\n", i, *(ins.PublicIpAddress))
+		fmt.Printf("\ninstance %d PrivateIpAddress: %s\n", i, *(ins.PrivateIpAddress))
+	}
+	//Reboot instance
+	for _, ins := range reservations[0].Instances {
+		rebootInput := &ec2.RebootInstancesInput{
+			InstanceIds: []*string{
+				aws.String(*(ins.InstanceId)),
+			},
+		}
+		fmt.Printf("Now reboot instance\n")
+		resp, err := svc.RebootInstances(rebootInput)
+		if err != nil {
+			fmt.Println("there was an error reboot instances", err.Error())
+			log.Fatal(err.Error())
+		}
+		fmt.Printf("%+v\n", resp)
+	}
 }
